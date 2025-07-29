3dc63cf18790b025c41df58fd5194349aba58951
Ruthwik Dhama <73566002+ruthwik-dhama@users.noreply.github.com>
2025-07-29 18:03:48 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import joblib
-from rapidfuzz import process  # faster than fuzzywuzzy
+from rapidfuzz import process
 import requests
 
 # ============================
